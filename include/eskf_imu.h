89cde3fdee68760d5fc10b77d086a09257fa7f3b
Hoang Quoc Hung CBNU <hoanghung21301580@gmail.com>
2025-07-14 15:16:52 +0900

    Add files via upload

--- include/eskf_imu.h
+++ include/eskf_imu.h
@@ -1,22 +1,23 @@
 #ifndef ESKF_IMU_H
 #define ESKF_IMU_H
 
-#include <ros/ros.h>
-#include <sensor_msgs/Imu.h>
-#include <sensor_msgs/MagneticField.h>
+#include <rclcpp/rclcpp.hpp>
+#include <sensor_msgs/msg/imu.hpp>
+#include <sensor_msgs/msg/magnetic_field.hpp>
 #include <message_filters/subscriber.h>
 #include <message_filters/time_synchronizer.h>
 #include <Eigen/Dense>
+#include <Eigen/Geometry>
 #include "butterworth.h"
 
 #define DEG2PI (M_PI / 180.0)
 
 struct Observation {
-  Eigen::Quaterniond quat;
-  Eigen::Vector3d euler;
+    Eigen::Quaterniond quat;
+    Eigen::Vector3d euler;
 };
 
-class ImuEskf {
+class ImuEskf : public rclcpp::Node {
 public:
     ImuEskf();
     Eigen::Matrix3d skew(const Eigen::Vector3d& x);
@@ -29,58 +30,58 @@ public:
     void error2nominal();
     Observation getData();
     double getTime() const;
-    void syncedCallback(const sensor_msgs::Imu::ConstPtr& imu_msg, const sensor_msgs::MagneticField::ConstPtr& mag_msg);
+    void syncedCallback(const sensor_msgs::msg::Imu::SharedPtr imu_msg, const sensor_msgs::msg::MagneticField::SharedPtr mag_msg);
 
 private:
-    ros::NodeHandle nh_;
-    message_filters::Subscriber<sensor_msgs::Imu> imu_sub_;
-    message_filters::Subscriber<sensor_msgs::MagneticField> mag_sub_;
-    message_filters::TimeSynchronizer<sensor_msgs::Imu, sensor_msgs::MagneticField> sync_;
-    ros::Publisher imu_pub_;
-    ros::Publisher accel_comp_pub_;
-    ros::Publisher gravity_pub_;
-    Eigen::VectorXd x_error_;     // Error state: [theta_x, theta_y, theta_z, wb_x, wb_y, wb_z]
-    Eigen::VectorXd x_nominal_;   // Nominal state: [qw, qx, qy, qz, wb_x, wb_y, wb_z]
-    Eigen::VectorXd z_;           // Measurement vector: [ax, ay, az, magx, magy, magz]
-    Eigen::MatrixXd P_;           // State covariance
-    Eigen::MatrixXd Fx_;          // State transition matrix
-    Eigen::MatrixXd H_;           // Measurement Jacobian
-    Eigen::MatrixXd Fi_;          // Noise input matrix
-    Eigen::MatrixXd Q_;           // Process noise covariance
-    Eigen::MatrixXd R_;           // Measurement noise covariance
-    Eigen::Vector3d a_ref_;       // Reference gravity (m/s²)
-    Eigen::Vector3d m_ref_;       // Reference magnetic field (unit vector)
-    double mag_norm_ref_;         // Expected magnetic field norm (uT)
-    Eigen::Vector3d mag_bias_;    // Magnetometer hard iron bias (uT)
-    Eigen::Vector3d raw_acc_;     // Accelerometer (unit vector)
-    Eigen::Vector3d raw_gyro_;    // Gyroscope (rad/s)
-    Eigen::Vector3d raw_mag_;     // Magnetometer (unit vector)
-    ros::Time last_time_;
-    sensor_msgs::Imu last_imu_msg_;
-    sensor_msgs::MagneticField last_mag_msg_;
-    ros::Time last_mag_time_;
+    rclcpp::Clock::SharedPtr clock_;
+    std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::Imu>> imu_sub_;
+    std::shared_ptr<message_filters::Subscriber<sensor_msgs::msg::MagneticField>> mag_sub_;
+    std::shared_ptr<message_filters::TimeSynchronizer<sensor_msgs::msg::Imu, sensor_msgs::msg::MagneticField>> sync_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr imu_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr accel_comp_pub_;
+    rclcpp::Publisher<sensor_msgs::msg::Imu>::SharedPtr gravity_pub_;
+    Eigen::VectorXd x_error_;
+    Eigen::VectorXd x_nominal_;
+    Eigen::VectorXd z_;
+    Eigen::MatrixXd P_;
+    Eigen::MatrixXd Fx_;
+    Eigen::MatrixXd H_;
+    Eigen::MatrixXd Fi_;
+    Eigen::MatrixXd Q_;
+    Eigen::MatrixXd R_;
+    Eigen::Vector3d a_ref_;
+    Eigen::Vector3d m_ref_;
+    double mag_norm_ref_;
+    Eigen::Vector3d mag_bias_;
+    Eigen::Vector3d raw_acc_;
+    Eigen::Vector3d raw_gyro_;
+    Eigen::Vector3d raw_mag_;
+    rclcpp::Time last_time_;
+    sensor_msgs::msg::Imu last_imu_msg_;
+    sensor_msgs::msg::MagneticField last_mag_msg_;
+    rclcpp::Time last_mag_time_;
     uint32_t update_count_;
     bool initialized_;
     bool euler_initialized_;
     bool use_magnetometer_;
     bool has_recent_mag_;
     bool noise_filter_;
-    bool filter_gyro_;           // Whether to filter gyroscope data when noise_filter is true
+    bool filter_gyro_;
     double current_t_;
     double dt_;
-    double gyro_noise_;           // Gyroscope noise variance
-    double gyro_bias_noise_;      // Gyroscope bias noise variance
-    double acc_noise_;            // Accelerometer noise variance
-    double mag_noise_;            // Magnetometer noise variance
-    double theta_noise_;          // Orientation error covariance
-    double wb_noise_;             // Gyroscope bias error covariance
-    Butter2 butter_ax_;           // Butterworth filter for accelerometer x
-    Butter2 butter_ay_;           // Butterworth filter for accelerometer y
-    Butter2 butter_az_;           // Butterworth filter for accelerometer z
-    Butter2 butter_wx_;           // Butterworth filter for gyroscope x
-    Butter2 butter_wy_;           // Butterworth filter for gyroscope y
-    Butter2 butter_wz_;           // Butterworth filter for gyroscope z
-    const int n_state_ = 6;       // Number of error state variables
+    double gyro_noise_;
+    double gyro_bias_noise_;
+    double acc_noise_;
+    double mag_noise_;
+    double theta_noise_;
+    double wb_noise_;
+    Butter2 butter_ax_;
+    Butter2 butter_ay_;
+    Butter2 butter_az_;
+    Butter2 butter_wx_;
+    Butter2 butter_wy_;
+    Butter2 butter_wz_;
+    const int n_state_ = 6;
 };
 
 #endif // ESKF_IMU_H
